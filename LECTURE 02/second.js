33c7023f2eb70d76b4a8dbc6d72b47714430addc
rahultheeagle <rahul01102004@gmail.com>
2026-08-06 18:19:16 +0530

    Lecture 2 completed

---
+++ LECTURE 02/second.js
@@ -0,0 +1,47 @@
+// declare the variable
+//let num = 10;
+//console.log(num); 
+
+// Primitive Data Type
+
+
+// Number, Boolean, String, Null, Undefined, Symbol, bigint
+
+// Number
+let account_balance = 20;
+console.log( account_balance);
+
+// String
+let str = "Rohit negi is a bad boy , he doesnot know how to use zoom";
+console.log( str);
+let comment = 'Hello doston';
+console.log(comment);
+
+// Boolean
+
+let papa_ko_block_kara_hai = true;
+console.log(papa_ko_block_kara_hai);
+console.log(typeof papa_ko_block_kara_hai);
+
+//Undefined
+let account;
+console.log(account);
+console.log(typeof account);
+
+//Null
+
+let balance = null;
+console.log(balance);
+console.log(typeof balance);
+
+//bigint
+let a = 1145641564151451654146514515n;
+console.log(a);
+
+ 
+console.log( Number.MAX_SAFE_INTEGER);
+console.log( Number.MIN_SAFE_INTEGER);
+
+
+// Non Primitive Data Type
+// Object, Array, Function
