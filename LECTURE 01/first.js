9121c70d65e00a7b955df8975600bc4539060cde
rahultheeagle <rahul01102004@gmail.com>
2026-08-06 15:28:34 +0530

    Lecture 01 done and dusted

---
+++ LECTURE 01/first.js
@@ -0,0 +1,23 @@
+console.log("Hello Coder Army");
+
+// let const var
+// var old
+
+let num = 10;
+console.log(num);
+
+
+
+let ar = "Rohit";
+ar  = "Mohit";
+console.log(ar);
+
+
+const ids = 20;
+console.log(ids);
+
+
+
+var x = 10;
+x = 20;
+console.log(x);
